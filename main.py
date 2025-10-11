f211a8187ba511a3746c1f3a776b15c311207f4b
My Compiler Hates Me <acjha990@gmail.com>
2025-10-11 21:10:57 +0530

    Added the python files from current stage of main project
    To ensure that the merger is clean.

---
+++ main.py
@@ -0,0 +1,8 @@
+from modules import web_search, text_reconstruction, report_generation
+
+fragment = input("Enter the fragmented information: ")
+reconstruction = text_reconstruction(fragment)
+source = web_search(fragment)
+report = report_generation(fragment, reconstruction, source)
+
+print(report)
